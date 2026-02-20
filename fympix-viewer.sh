431b62805e3cf53bab56788c6c0dea980591a9ea
fymmit <blomqvist.timo@gmail.com>
2026-02-20 14:05:51 +0200

    Allow search terms

--- fympix-viewer.sh
+++ fympix-viewer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-pic=$(curl -fsS "https://api.fympix.com" | jq -r '.[]' | shuf -n 1)
+search="$1"
+
+pic=$(curl -fsS "https://api.fympix.com?search=$search" | jq -r '.[]' | shuf -n 1)
 
 curl -fsSL "$pic" | chafa
 
