364d622d9f538d8c2f03580565a66c4970d6f572
fymmit <blomqvist.timo@gmail.com>
2026-03-01 07:20:02 +0200

    Niri-window-picker try to automatically open program if window not found

--- niri-window-picker.sh
+++ niri-window-picker.sh
@@ -4,10 +4,11 @@
 # https://github.com/niri-wm/niri/discussions/1096#discussion-7929601
 
 window_list=$(niri msg --json windows)
-name=$(echo $window_list | jq -r '.[] | ."title"' | fuzzel --dmenu)
-id=$(echo $window_list | jq -r '.[] | ."title", ."id"' | grep "$name" -x -A 1 | grep "$name" -vx)
+name=$(echo $window_list | jq -r '.[] | ."title"+" | "+."app_id"' | fuzzel --dmenu)
+id=$(echo $window_list | jq -r '.[] | ."title"+" | "+."app_id", ."id"' | grep "$name" -x -A 1 | grep "$name" -vx)
 
-echo $id
-echo $name
-
-niri msg action focus-window --id $id
+if [[ -z "$id" ]] && [[ -n "$name" ]]; then
+    fuzzel --search="$name" --auto-select
+else
+    niri msg action focus-window --id $id
+fi
