219a3e07c2fb0e7b68ce9998aec43bc3f7332e1d
fymmit <blomqvist.timo@gmail.com>
2026-03-01 07:20:34 +0200

    Now playing escapes ampersands properly

--- now-playing.sh
+++ now-playing.sh
@@ -2,7 +2,7 @@
 
 playerctl metadata --format '{{ status }}|{{ artist }} - {{ title }}' --follow | while IFS="|" read -r status song; do
     case "$status" in
-        Playing) echo " $song" ;;
+        Playing) echo " $song" | sed 's/&/&amp;/g' ;;
         *) echo "" ;;
     esac
 done
