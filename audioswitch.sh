6fbd00aed6371e4c48d3c4ba6566900c5b56ea76
fymmit <blomqvist.timo@gmail.com>
2026-03-16 06:46:48 +0200

    Add mic mute toggle to audioswitch

--- audioswitch.sh
+++ audioswitch.sh
@@ -5,13 +5,18 @@
 
 sink=$(pactl get-default-sink)
 
-selection=$(echo -e "Headphones\nSpeakers" | fuzzel --anchor=top-right -d --hide-prompt)
+selection=$(echo -e "Headphones\nSpeakers\nMic toggle" | fuzzel --anchor=top-right -d --hide-prompt)
 
 if [ "$selection" == "Headphones" ]; then
     pactl set-sink-port "$sink" analog-output-headphones
 elif [ "$selection" == "Speakers" ]; then
     pactl set-sink-port "$sink" analog-output-lineout
+elif [[ "$selection" == "Mic toggle" ]]; then
+    wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
+    selection=""
 fi
+
+[[ -n "$selection" ]] && echo "$selection" > ~/script-data/audio_info.txt
 
 pkill -x -RTMIN+10 waybar 2>/dev/null
 
