54221d3b3630609976dd674f40cceb5b66fbb7f5
fymmit <blomqvist.timo@gmail.com>
2026-03-20 13:02:57 +0200

    Add HDMI support to audioswitch

--- audioswitch.sh
+++ audioswitch.sh
@@ -3,14 +3,19 @@
 # Check ports with this
 # pactl list sinks |& grep -E "Sink|Ports|analog-ou"
 
-sink=$(pactl get-default-sink)
+analog_stereo_sink=$(pactl list sinks short | awk '/analog-stereo/ {print $2}')
+hdmi_sink=$(pactl list sinks short | awk '/hdmi/ {print $2}')
 
-selection=$(echo -e "Headphones\nSpeakers\nMic toggle" | fuzzel --anchor=top-right -d --hide-prompt)
+selection=$(echo -e "Headphones\nSpeakers\nHDMI\nMic toggle" | fuzzel --anchor=top-right -d --hide-prompt)
 
 if [ "$selection" == "Headphones" ]; then
-    pactl set-sink-port "$sink" analog-output-headphones
+    pactl set-default-sink "$analog_stereo_sink"
+    pactl set-sink-port "$analog_stereo_sink" analog-output-headphones
 elif [ "$selection" == "Speakers" ]; then
-    pactl set-sink-port "$sink" analog-output-lineout
+    pactl set-default-sink "$analog_stereo_sink"
+    pactl set-sink-port "$analog_stereo_sink" analog-output-lineout
+elif [ "$selection" == "HDMI" ]; then
+    pactl set-default-sink "$hdmi_sink"
 elif [[ "$selection" == "Mic toggle" ]]; then
     wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
     selection=""
